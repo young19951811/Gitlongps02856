6b91177e42884d92fb3803a810df710c0bcba53c
Bảo Long Trương <young19951811@live.com>
2016-02-04 13:13:20 +0700

    Edit Loading

--- Cloud_Ps02856/frmloading.vb
+++ Cloud_Ps02856/frmloading.vb
@@ -1,3 +1,16 @@
 ﻿Public Class frmloading
+    Private Sub Loading_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        timer.Start()
 
+    End Sub
+
+    Private Sub timer_Tick(sender As Object, e As EventArgs) Handles timer.Tick
+        prbloading.Increment(10)
+        If prbloading.Value = 100 Then
+            timer.Stop()
+            frmdash.Show()
+            Me.Close()
+
+        End If
+    End Sub
 End Class
