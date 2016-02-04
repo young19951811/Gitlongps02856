d17febe02a7b62821bcda083ff7406f75026959c
Bảo Long Trương <young19951811@live.com>
2016-02-04 13:11:25 +0700

    Edit Dash

--- Cloud_Ps02856/frmdash.vb
+++ Cloud_Ps02856/frmdash.vb
@@ -1,5 +1,17 @@
 ﻿Public Class frmdash
-    Private Sub frmdash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub ThoátToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ThoátToolStripMenuItem.Click
+        Me.Close()
+    End Sub
 
+    Private Sub msiquanlykhach_Click(sender As Object, e As EventArgs) Handles msiquanlykhach.Click
+        frmkhach.Show()
+    End Sub
+
+    Private Sub msiquanlysanpham_Click(sender As Object, e As EventArgs) Handles msiquanlysanpham.Click
+        frmsp.Show()
+    End Sub
+
+    Private Sub msithoat_Click(sender As Object, e As EventArgs) Handles msithoat.Click
+        frmhoadon.Show()
     End Sub
 End Class
